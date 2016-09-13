e878438dad93a97e1a17fb317ce394999bb3606e
kvaxminsk <korol1011@gmail.cpm>
2016-09-13 17:35:13 +0300

    2

--- app/assets/AppAsset.php
+++ app/assets/AppAsset.php
@@ -6,7 +6,7 @@ class AppAsset extends \yii\web\AssetBundle
     public $sourcePath = '@app/media';
     public $css = [
         'css/styles.css',
-        'css/styles-new.css',
+        'css/style-new.css',
         'css/components.css',
     ];
     public $js = [
--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -59,7 +59,8 @@ $asset = \app\assets\AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700|Roboto:300,400,700" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css" rel="stylesheet">
     <title><?= Html::encode($this->title) ?></title>
     <!--    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700|Roboto:300,400,700" rel="stylesheet">-->
     <!--    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css" rel="stylesheet">-->
